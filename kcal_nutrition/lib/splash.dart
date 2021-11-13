394bb043593399e67e1b1cb2ccbdefe100f01c55
Martin <Lapetajemartin@gmail.com>
2021-11-14 00:23:28 +0800

    splash screen: done

--- kcal_nutrition/lib/splash.dart
+++ kcal_nutrition/lib/splash.dart
@@ -1,56 +1,77 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'welcome.dart';
 
-class Splash extends StatelessWidget {
-  const Splash({Key? key}) : super(key: key);
+class Splash extends StatefulWidget {
+  Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: 'Welcome',
+                )),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF91C789),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/bgStickers.png"),
-            fit: BoxFit.cover,
+      backgroundColor: Color(0xFF91C789),
+      body: AnimatedOpacity(
+        // If the widget is visible, animate to 0.0 (invisible).
+        // If the widget is hidden, animate to 1.0 (fully visible).
+        opacity: 0.0,
+        duration: Duration(milliseconds: 2000),
+
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bgStickers.png"),
+              fit: BoxFit.cover,
+            ),
           ),
+          child: Stack(children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'kcal',
+                style: GoogleFonts.nunito(
+                  fontSize: 72,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.8),
+              child: Text(
+                'LAPETAJE',
+                style: GoogleFonts.nunito(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white54,
+                ),
+              ),
+            )
+          ]),
         ),
-        child: Stack(children: [
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              'kcal',
-              style: GoogleFonts.nunito(
-                fontSize: 72,
-                fontWeight: FontWeight.w800,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment(0.0, 0.8),
-            child: Text(
-              'LAPETAJE',
-              style: GoogleFonts.nunito(
-                fontSize: 28,
-                fontWeight: FontWeight.w800,
-                color: Colors.white54,
-              ),
-            ),
-          )
-        ]),
       ),
     );
   }
 }
-
-//  children: Text(
-//           'kcal',
-//           style: GoogleFonts.nunito(
-//             fontSize: 72,
-//             fontWeight: FontWeight.w800,
-//             color: Colors.white,
-//           ),
-//         ),

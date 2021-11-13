7614ca78ff21a4cfb169e089c9e5e6c62cebee37
Martin <Lapetajemartin@gmail.com>
2021-11-13 22:50:57 +0800

    splash screen: done

--- kcal_nutrition/lib/splash.dart
+++ kcal_nutrition/lib/splash.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,16 +10,47 @@ class Splash extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF91C789),
-      body: Center(
-        child: Text(
-          'kcal',
-          style: GoogleFonts.nunito(
-            fontSize: 72,
-            fontWeight: FontWeight.w800,
-            color: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bgStickers.png"),
+            fit: BoxFit.cover,
           ),
         ),
+        child: Stack(children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'kcal',
+              style: GoogleFonts.nunito(
+                fontSize: 72,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, 0.8),
+            child: Text(
+              'LAPETAJE',
+              style: GoogleFonts.nunito(
+                fontSize: 28,
+                fontWeight: FontWeight.w800,
+                color: Colors.white54,
+              ),
+            ),
+          )
+        ]),
       ),
     );
   }
 }
+
+//  children: Text(
+//           'kcal',
+//           style: GoogleFonts.nunito(
+//             fontSize: 72,
+//             fontWeight: FontWeight.w800,
+//             color: Colors.white,
+//           ),
+//         ),

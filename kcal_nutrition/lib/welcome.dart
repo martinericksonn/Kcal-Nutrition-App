1b94475e0403bc4c2f2566fb34d408c2842b5183
Martin <Lapetajemartin@gmail.com>
2021-11-15 20:09:09 +0800

    Welcome screen: carsousel slide done

--- kcal_nutrition/lib/welcome.dart
+++ kcal_nutrition/lib/welcome.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _current = 0;
   final CarouselController _controller = CarouselController();
   TextStyle appName = GoogleFonts.nunito(
-    fontSize: 32,
+    fontSize: 38,
     fontWeight: FontWeight.w800,
     color: Color(0xFF91C789),
   );
@@ -108,90 +108,36 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Align(
-                alignment: Alignment(0.0, 0.4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: imgList.asMap().entries.map((entry) {
-                    return GestureDetector(
-                      onTap: () => _controller.animateToPage(entry.key),
-                      child: Container(
-                        width: 21.0,
-                        height: 13.0,
-                        margin: EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 1.5),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.elliptical(140, 100)),
-                            color:
-                                (Theme.of(context).brightness == Brightness.dark
-                                        ? Colors.white
-                                        : Color(0xFFFF9386))
-                                    .withOpacity(
-                                        _current == entry.key ? 0.9 : 0.4)),
-                      ),
-                    );
-                  }).toList(),
-                ))
+              alignment: Alignment(0.0, 0.4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    // onTap: () => _controller.animateToPage(entry.key),
+                    child: Container(
+                      width: _current == entry.key ? 20.0 : 12,
+                      height: _current == entry.key ? 12.0 : 8,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 1.5),
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.all(Radius.elliptical(140, 100)),
+                          color: (Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Color(0xFFFF9386))
+                              .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            Align(
+                alignment: Alignment(0, 5.5),
+                child: TextButton(onPressed: () {}, child: Text('TextButton'))),
           ],
         ),
       ),
-    );
-  }
-}
-
-class CarouselWithIndicatorDemo extends StatefulWidget {
-  const CarouselWithIndicatorDemo({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _CarouselWithIndicatorState();
-  }
-}
-
-class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
-  int _current = 0;
-  final CarouselController _controller = CarouselController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Carousel with indicator controller demo')),
-      body: Column(children: [
-        Expanded(
-          child: CarouselSlider(
-            items: imageSliders,
-            carouselController: _controller,
-            options: CarouselOptions(
-                autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 2.0,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _current = index;
-                  });
-                }),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.asMap().entries.map((entry) {
-            return GestureDetector(
-              onTap: () => _controller.animateToPage(entry.key),
-              child: Container(
-                width: 12.0,
-                height: 12.0,
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: (Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black)
-                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-              ),
-            );
-          }).toList(),
-        ),
-      ]),
     );
   }
 }

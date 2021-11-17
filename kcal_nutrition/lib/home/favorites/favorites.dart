a319b04fc27caa5e199c3e12a3acc234eae54eca
Martin <Lapetajemartin@gmail.com>
2021-11-17 17:03:21 +0800

    favorite screen: radi nav bar

--- kcal_nutrition/lib/home/favorites/favorites.dart
+++ kcal_nutrition/lib/home/favorites/favorites.dart
@@ -12,6 +12,16 @@ class AppHome extends StatefulWidget {
 }
 
 class _AppHomeState extends State<AppHome> {
+  int _selectedIndex = 0;
+  Color selectedIcon = BLACK;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      selectedIcon = PINK;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,47 +30,70 @@ class _AppHomeState extends State<AppHome> {
           child: Text("Hello World"),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        // ignore: prefer_const_literals_to_create_immutables
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Feather.home,
-              color: BLACK,
-            ),
-            label: 'home',
+      bottomNavigationBar: SizedBox(
+        height: 62,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            // ignore: prefer_const_literals_to_create_immutables
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                spreadRadius: 1,
+                blurRadius: 0.1,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Feather.search,
-              color: BLACK,
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
             ),
-            label: 'search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Feather.camera,
-              color: BLACK,
+            child: BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              // ignore: prefer_const_literals_to_create_immutables
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Feather.home,
+                  ),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Feather.search,
+                  ),
+                  label: 'search',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Feather.camera,
+                  ),
+                  label: 'camera',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Feather.heart,
+                  ),
+                  label: 'heart',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Feather.user,
+                  ),
+                  label: 'user',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: PINK,
+              unselectedItemColor: BLACK,
+              onTap: _onItemTapped,
+              type: BottomNavigationBarType.fixed,
             ),
-            label: 'camera',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Feather.heart,
-              color: BLACK,
-            ),
-            label: 'heart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Feather.user,
-              color: BLACK,
-            ),
-            label: 'user',
-          ),
-        ],
+        ),
       ),
     );
   }

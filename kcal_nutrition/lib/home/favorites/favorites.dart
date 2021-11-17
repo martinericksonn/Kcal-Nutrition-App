09e3c767eb669f1999426bb5a50f94f453afea1b
Martin <Lapetajemartin@gmail.com>
2021-11-17 16:17:10 +0800

    favorite screen: added final icons

--- kcal_nutrition/lib/home/favorites/favorites.dart
+++ kcal_nutrition/lib/home/favorites/favorites.dart
@@ -1,10 +1,17 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:flutter/material.dart'
+// ignore_for_file: prefer_const_constructors, import_of_legacy_library_into_null_safe, prefer_const_constructors_in_immutables
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter/material.dart';
 import 'package:kcal_nutrition/constans.dart';
+// import 'package:octicons_icon/octicons.dart';
 
-class AppHome extends StatelessWidget {
-  const AppHome({Key? key}) : super(key: key);
+class AppHome extends StatefulWidget {
+  AppHome({Key? key}) : super(key: key);
 
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,41 +23,42 @@ class AppHome extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        items: const [
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.home_outlined,
+              Feather.home,
               color: BLACK,
             ),
-            label: 'Home',
+            label: 'home',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.search_rounded,
+              Feather.search,
               color: BLACK,
             ),
-            label: 'School',
+            label: 'search',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.photo_camera_rounded,
+              Feather.camera,
               color: BLACK,
             ),
-            label: 'null',
+            label: 'camera',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.favorite_border_rounded,
+              Feather.heart,
               color: BLACK,
             ),
-            label: 'null',
+            label: 'heart',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.person_outline_rounded,
+              Feather.user,
               color: BLACK,
             ),
-            label: 'null',
+            label: 'user',
           ),
         ],
       ),

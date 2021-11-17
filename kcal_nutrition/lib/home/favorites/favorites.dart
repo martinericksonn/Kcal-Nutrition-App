906d626f70a2a9d3e82617aae1c492cad5b36b77
Martin <Lapetajemartin@gmail.com>
2021-11-17 14:43:24 +0800

    favorite screen: added icons

--- kcal_nutrition/lib/home/favorites/favorites.dart
+++ kcal_nutrition/lib/home/favorites/favorites.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
 import 'package:kcal_nutrition/constans.dart';
 
 class AppHome extends StatelessWidget {
